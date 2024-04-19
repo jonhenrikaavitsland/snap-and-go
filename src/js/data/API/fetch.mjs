a925d7733bfff3dc323a59647a6d3be02e3b5dfb
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-19 13:24:27 +0200

    function to fetch data from api sources

---
+++ src/js/data/API/fetch.mjs
@@ -0,0 +1,17 @@
+/**
+ * The object is used to fetch information from an external source such as an API.
+ * @param {string} url
+ * @param {object} object
+ * @returns {*} data from the API, often in the form of an array.
+ */
+
+export async function fetchData(url, object) {
+  const response = await fetch(url, object);
+  const result = await response.json();
+
+  if (!response.ok) {
+    // thro error
+  }
+
+  return result;
+}
