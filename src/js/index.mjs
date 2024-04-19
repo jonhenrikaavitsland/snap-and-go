1e770b497a8172d873d7184963193f6e61f6e8fe
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-19 13:22:57 +0200

    swapping to mjs file extension

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,4 @@
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js";
-import { router } from "./router/router.js";
+import { router } from "./router/router.mjs";
 
 router();
