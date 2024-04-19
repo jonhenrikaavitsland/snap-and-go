4c282a6d79c48ebc9ffc9cfc584e5b1eadd31038
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-19 20:52:25 +0200

    removing comments

--- src/js/data/API/registerUser.mjs
+++ src/js/data/API/registerUser.mjs
@@ -1,23 +1,6 @@
 import { API_AUIH, API_BASE, API_REGISTER } from "./constants.mjs";
 import { fetchData } from "./fetch.mjs";
 
-// export async function registerUser(name, email, password) {
-//   const response = await fetch(`${API_BASE}${API_AUIH}${API_REGISTER}`, {
-//     headers: {
-//       "Content-Type": "application/json; charset=UTF-8",
-//     },
-//     method: "POST",
-//     body: JSON.stringify({ name, email, password }),
-//   });
-
-//   if (!response.ok) {
-//     throw new Error("Could not register the account");
-//   }
-
-//   const data = await response.json();
-//   return data;
-// }
-
 /**
  * The function will send a POST request to the API and try to register new user
  * @param {string} name
