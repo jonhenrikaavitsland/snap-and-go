ab8c44eab6677bd2fffb87ba70b9bbb185709b1b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-19 21:59:55 +0200

    saved to its own file

---
+++ src/js/data/API/getPostsWithImage.mjs
@@ -0,0 +1,23 @@
+import { load } from "../../localStorage/load.mjs";
+import { API_BASE, API_KEY, API_POSTS } from "./constants.mjs";
+import { fetchData } from "./fetch.mjs";
+
+export async function getPostsWithImages() {
+  const postsWithImages = [];
+
+  for (let i = 0; i < 15; i++) {
+    const posts = await fetchData(`${API_BASE}${API_POSTS}?page=${i + 1}`, {
+      headers: {
+        Authorization: `Bearer ${load("token")}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
+    });
+
+    for (let y = 0; y < posts.length; y++) {
+      if (posts[y].media.length >= 10) {
+        postsWithImages.push(posts[y]);
+      }
+    }
+  }
+  return postsWithImages;
+}
