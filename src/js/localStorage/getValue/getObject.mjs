b86b7cbd83663aee216591bcacdeef3de0046435
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-04-19 21:44:36 +0200

    jsdocs

--- src/js/localStorage/getValue/getObject.mjs
+++ src/js/localStorage/getValue/getObject.mjs
@@ -2,6 +2,14 @@
 
 import { load } from "../load.mjs";
 
+/**
+ * Retrieves the user profile object from storage.
+ *
+ * This function fetches the user's profile data by calling `load` with the key "profile".
+ * It returns the retrieved object.
+ *
+ * @returns {Object} The user profile object loaded from storage.
+ */
 export function getObject() {
   const object = load("profile");
   console.log(object);
