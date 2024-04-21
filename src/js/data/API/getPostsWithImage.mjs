72edd4603d36650c551c638cc331d1334b758970
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-21 04:33:20 +0200

    now including authors, comments and reactions

--- src/js/data/API/getPostsWithImage.mjs
+++ src/js/data/API/getPostsWithImage.mjs
@@ -6,7 +6,7 @@ export async function getPostsWithImages() {
   const postsWithImages = [];
 
   for (let i = 0; i < 2; i++) {
-    let posts = await fetchData(`${API_BASE}${API_POSTS}?page=${i + 1}`, {
+    let posts = await fetchData(`${API_BASE}${API_POSTS}?_author=true&_comments=true&_reactions=true&?page=${i + 1}`, {
       headers: {
         Authorization: `Bearer ${load("token")}`,
         "X-Noroff-API-Key": API_KEY,
