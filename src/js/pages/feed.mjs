bf606d5ec6abb8de8ee1a8c820bed0bf5683c1f9
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-22 19:26:16 +0200

    testing

--- src/js/pages/feed.mjs
+++ src/js/pages/feed.mjs
@@ -1,11 +1,9 @@
-import { getPosts } from "../data/API/getPosts.mjs";
 import { postsWithTags, postsWithTagsChunks } from "../data/posts.mjs";
 import { renderCards } from "../render/renderCards.mjs";
 import { createPost } from "../ui/createPost/createPost.mjs";
 
-console.log(postsWithTags);
-console.log(postsWithTagsChunks);
-console.log("Last 100:", await getPosts());
+console.log("Posts W Tag", postsWithTags);
+console.log("Posts chunks", postsWithTagsChunks);
 
 createPost();
 renderCards(postsWithTagsChunks[0]);
