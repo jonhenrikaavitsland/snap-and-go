0eee4cf3e2df8c7a3d07fd5983a43dbb61041663
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-22 19:26:07 +0200

    new function to get posts and then filter out the ones we want

--- src/js/data/API/getPostsWithTags.mjs
+++ src/js/data/API/getPostsWithTags.mjs
@@ -3,37 +3,22 @@ import { API_BASE, API_KEY, API_POSTS } from "./constants.mjs";
 import { fetchData } from "./fetch.mjs";
 
 export async function getPostsWithTags() {
-  const postsWithTag = [];
-  console.log("TEST:", postsWithTag);
+  const newArray = [];
 
-  for (let i = 0; i < 4; i++) {
-    const posts = await fetchData(`${API_BASE}${API_POSTS}?_author=true&_comments=true&_reactions=true&?page=${i + 1}`, {
+  for (let i = 0; i < 3; i++) {
+    const posts = await fetchData(`${API_BASE}${API_POSTS}?page=${i + 1}&_author=true&_comments=true&_reactions=true`, {
       headers: {
         Authorization: `Bearer ${load("token")}`,
         "X-Noroff-API-Key": API_KEY,
       },
     });
 
-    posts.data.forEach(element => {
-      if (posts.data[0].tags[0] === "snapgo") {
-        postsWithTag.push(posts.data[0]);
-      }
+    posts.data.forEach(post => {
+      newArray.push(post);
     });
-
-    // for (let y = 0; y < posts.data.length; y++) {
-    //   if (posts.data[y].tags[0] === "snapgo") {
-    //     postsWithTag.push(posts.data[y]);
-    //   }
-    // }
-
-    console.log("TEST:", postsWithTag);
-
-    // console.log(posts);
-
-    // if (posts.data[0].tags[0] === "snapgo") {
-    //   postsWithTag.push(posts.data[i]);
-    // }
   }
-  console.log("TEST:", postsWithTag);
+
+  const postsWithTag = newArray.filter(post => post.tags[0] === "snapgo");
+
   return postsWithTag;
 }
