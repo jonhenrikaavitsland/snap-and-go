2858a2c3ab78ec8778cf357ae63fba3bbb634f2f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-22 10:32:54 +0200

    needs rework

--- src/js/data/API/getPostsWithTags.mjs
+++ src/js/data/API/getPostsWithTags.mjs
@@ -4,18 +4,36 @@ import { fetchData } from "./fetch.mjs";
 
 export async function getPostsWithTags() {
   const postsWithTag = [];
+  console.log("TEST:", postsWithTag);
 
-  for (let i = 0; i < 2; i++) {
-    let posts = await fetchData(`${API_BASE}${API_POSTS}?_author=true&_comments=true&_reactions=true&?page=${i + 1}`, {
+  for (let i = 0; i < 4; i++) {
+    const posts = await fetchData(`${API_BASE}${API_POSTS}?_author=true&_comments=true&_reactions=true&?page=${i + 1}`, {
       headers: {
         Authorization: `Bearer ${load("token")}`,
         "X-Noroff-API-Key": API_KEY,
       },
     });
 
-    if (posts.data[i].tags[0] === "snapgo") {
-      postsWithTag.push(posts.data[i]);
-    }
+    posts.data.forEach(element => {
+      if (posts.data[0].tags[0] === "snapgo") {
+        postsWithTag.push(posts.data[0]);
+      }
+    });
+
+    // for (let y = 0; y < posts.data.length; y++) {
+    //   if (posts.data[y].tags[0] === "snapgo") {
+    //     postsWithTag.push(posts.data[y]);
+    //   }
+    // }
+
+    console.log("TEST:", postsWithTag);
+
+    // console.log(posts);
+
+    // if (posts.data[0].tags[0] === "snapgo") {
+    //   postsWithTag.push(posts.data[i]);
+    // }
   }
+  console.log("TEST:", postsWithTag);
   return postsWithTag;
 }
