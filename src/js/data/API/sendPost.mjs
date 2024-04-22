fed87255639d28cb8514694da016cd8c680e62fe
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-22 09:28:56 +0200

    send post to API

---
+++ src/js/data/API/sendPost.mjs
@@ -0,0 +1,16 @@
+import { API_BASE, API_POSTS, API_KEY } from "./constants.mjs";
+import { fetchData } from "./fetch.mjs";
+import { load } from "../../localStorage/load.mjs";
+
+export async function sendPost(title, tags, media) {
+  const postResponse = await fetchData(`${API_BASE}${API_POSTS}`, {
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${load("token")}`,
+      "X-Noroff-API-Key": API_KEY,
+    },
+    method: "POST",
+    body: JSON.stringify({ title, tags, media }),
+  });
+  return postResponse;
+}
