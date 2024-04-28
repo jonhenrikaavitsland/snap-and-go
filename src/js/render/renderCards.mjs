42053c89ad93735c88376ecab941070970ceb80b
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-28 08:47:07 +0200

    corrected spelling

--- src/js/render/renderCards.mjs
+++ src/js/render/renderCards.mjs
@@ -3,7 +3,7 @@ import { createCard } from "../ui/postCard/createCard.mjs";
 /**
  * A function to render the cards in the feed
  * It will clear parent before rendering cards via the createCard function.
- * @param {obnject} cards 
+ * @param {object} cards 
  */
 export function renderCards(cards, parent) {
   cards.forEach(element => {
