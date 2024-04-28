4bf5d5e8ab7cd3d9da91908ed90f72f75a4f8406
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-28 09:10:08 +0200

    JSDocs

--- src/js/ui/viewMoreBtnFeedPage/buttonAction.mjs
+++ src/js/ui/viewMoreBtnFeedPage/buttonAction.mjs
@@ -2,6 +2,17 @@ import { postsWithTagsChunks } from "../../data/posts.mjs";
 import { renderCards } from "../../render/renderCards.mjs";
 import { viewMoreBtn } from "./viewMoreBtn.mjs";
 
+/**
+ * Decides what should happen when the button is pressed
+ * @param {array} array 
+ * @param {string} classname 
+ * @param {string} parent 
+ * @param {string} btnText 
+ * @param {string} btnTheme 
+ * When clicked, saving the number of arrays to load in. 
+ * Then renders posts based on the number of arrays. 
+ * Then as long as the number of arrays is not maxed, renders the view more button.
+ */
 export function buttonAction(array, classname, parent, btnText, btnTheme) {
   let length = array.length;
   let currentPos = 1;
--- src/js/ui/viewMoreBtnFeedPage/viewMoreBtn.mjs
+++ src/js/ui/viewMoreBtnFeedPage/viewMoreBtn.mjs
@@ -1,5 +1,12 @@
 import { renderBtn } from "../../render/renderBtn.mjs";
 
+/**
+ * Renders a button
+ * @param {string} text 
+ * @param {string} theme 
+ * @param {string} classname 
+ * @param {string} parent 
+ */
 export function viewMoreBtn(text, theme, classname, parent) {
   parent.append(renderBtn(text, theme, classname));
 }
