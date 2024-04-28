4de3789d1dbb1350db356f3f7693073a9266dc0f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-28 09:11:45 +0200

    JSDocs

--- src/js/ui/createPost/checkTextInputLength.mjs
+++ src/js/ui/createPost/checkTextInputLength.mjs
@@ -1,3 +1,9 @@
+/**
+ * A function to check if input value is too long.
+ * @param {object} input
+ * @param {number} length
+ * @returns alert if limit was reached!
+ */
 export function checkTextInputLength(input, length) {
   if (input.length > length) {
     alert(`Text exceeds the ${length} characters limit.`);
