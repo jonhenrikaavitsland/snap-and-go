aa0a1fdab6f8334ed9b5ce02ba4f8c1b59c9ac09
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-04-28 08:47:36 +0200

    removed consol logs

--- src/js/pages/feed.mjs
+++ src/js/pages/feed.mjs
@@ -6,9 +6,6 @@ import { viewMoreBtn } from "../ui/viewMoreBtnFeedPage/viewMoreBtn.mjs";
 
 const parent = document.querySelector(".feed-container");
 
-console.log("Posts W Tag", postsWithTags);
-console.log("Posts chunks", postsWithTagsChunks);
-
 parent.innerHTML = "";
 renderCards(postsWithTagsChunks[0], parent);
 viewMoreBtn("View more", "secondary", "view-more-btn", parent);
