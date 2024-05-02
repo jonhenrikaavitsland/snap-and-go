c91d35101f209d7773b5fd325431542956f703bd
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-02 11:59:24 +0200

    changed the function

--- src/js/errorHandling/validate/sanitizePassword.mjs
+++ src/js/errorHandling/validate/sanitizePassword.mjs
@@ -1,19 +1,31 @@
 /**
- * Number or string containing number
- * @typedef {(number|string)} NumberLike
+ * Check a password to ensure there is no special characters and that it meets the requirement of minimum 8 characters.
+ * @param {string} passwordRegex
+ * @returns true or false
  */
+export function sanitizePassword(passwordRegex) {
+  const passwordField = document.querySelector("#inputPassword");
+  const passwordError = document.querySelector(".invalid-feedback-pw");
+  const passwordValid = document.querySelector(".valid-feedback-pw");
+  let validState = false;
 
-/**
- * This function will test if the provided password does not contain any special characters.
- * @param {NumberLike} password
- * @returns a trimmed password if successful and null if not successful
- */
-export function sanitizePassword(password) {
-  const passwordRegex = /^[a-zA-Z0-9]*$/;
-
-  if (passwordRegex.test(password)) {
-    return password.trim();
+  if (passwordField.value.trim() !== "" && !passwordRegex.test(passwordField.value.trim())) {
+    passwordValid.style.display = "none";
+    passwordError.style.display = "block";
+    validState = false;
   } else {
-    return null;
+    passwordError.style.display = "none";
+    passwordValid.style.display = "block";
+    validState = true;
   }
+  if (passwordField.value.trim() !== "" && passwordField.value.trim().length < 8) {
+    passwordValid.style.display = "none";
+    passwordError.style.display = "block";
+    validState = false;
+  }
+  if (passwordField.value.trim() === "") {
+    passwordValid.style.display = "none";
+    validState = false;
+  }
+  return validState;
 }
