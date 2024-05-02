7663d6f96ce5c3b2fc3e979d6a7953795f1cfb24
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-02 18:01:54 +0200

    removing commented code

--- src/js/data/API/registerAuth.mjs
+++ src/js/data/API/registerAuth.mjs
@@ -1,28 +1,6 @@
 import { validateUser } from "../../errorHandling/validate/validateUser.mjs";
 import { registerUser } from "./registerUser.mjs";
 
-/**
- * This function is used to authenticate the registration of a new user.
- * If the registration was successful, the user is redirected to the root.
- */
-// export async function registerAuth() {
-//   const form = document.querySelector("form");
-//   form.addEventListener("submit", async event => {
-//     event.preventDefault();
-//     const name = event.target.email.value.replace(/@.*$/, "");
-//     const email = event.target.email.value;
-//     let password;
-
-//     if (event.target.password.value === event.target.repeatPassword.value) {
-//       password = event.target.password.value;
-//     } else {
-//       throw new Error("Password must match");
-//     }
-//     await registerUser(name, email, password);
-
-//     window.location.href = "/";
-//   });
-// }
 
 export function registerAuth() {
   const form = document.querySelector("form");
