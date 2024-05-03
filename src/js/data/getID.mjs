ca45e5147d6bf071fe019926d0a4bc6fd5f837aa
RamonaXR <Ramonaelise@gmail.com>
2024-05-03 12:51:33 +0200

    updating function

--- src/js/data/getID.mjs
+++ src/js/data/getID.mjs
@@ -4,7 +4,7 @@
  * @returns {string|null} The post ID and null if not found.
  */
 
-export function getID() {
+export function getID(param) {
     const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get("id");
+    return urlParams.get(param);
 }
