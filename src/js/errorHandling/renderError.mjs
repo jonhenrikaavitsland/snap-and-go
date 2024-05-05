0afead8b39530522a01be3b930ccaa8b69616c47
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-05 17:04:50 +0200

    makes sure we only render error once

--- src/js/errorHandling/renderError.mjs
+++ src/js/errorHandling/renderError.mjs
@@ -3,8 +3,14 @@ import { errorRouter } from "./errorRouter.mjs";
 export function renderError(error, parent) {
   const currentError = errorRouter(error);
 
+  const existingError = document.querySelector(".error");
+
+  if (existingError) {
+    existingError.outerHTML = "";
+  }
+
   const errorContainer = document.createElement("div");
-  errorContainer.classList.add("mt-3", "p-3", "text-danger", "border", "border-1", "border-danger", "rounded");
+  errorContainer.classList.add("error", "mt-3", "p-3", "text-danger", "border", "border-1", "border-danger", "rounded");
 
   const errorText = document.createElement("p");
   errorText.textContent = currentError;
