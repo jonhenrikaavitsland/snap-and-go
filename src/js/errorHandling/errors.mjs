d38be0652623090a972c809a89bc842aac6bea9f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-05 15:54:20 +0200

    list of errors

---
+++ src/js/errorHandling/errors.mjs
@@ -0,0 +1,2 @@
+export const ERROR_API = new Error("Oops! Something went wrong trying to fetch the API.");
+export const ERROR_API_400 = new Error("Bad request, please ensure that your inputs are correct.");
