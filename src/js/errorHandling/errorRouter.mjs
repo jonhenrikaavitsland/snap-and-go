1a701f3b47b3bd70a9f5ad8a1b9bb069bc941b5f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-05 17:04:21 +0200

    including case 401

--- src/js/errorHandling/errorRouter.mjs
+++ src/js/errorHandling/errorRouter.mjs
@@ -1,9 +1,11 @@
-import { ERROR_API, ERROR_API_400 } from "./errors.mjs";
+import { ERROR_API, ERROR_API_400, ERROR_API_401 } from "./errors.mjs";
 
 export function errorRouter(error) {
   switch (error) {
     case 400:
       return ERROR_API_400;
+    case 401:
+      return ERROR_API_401;
     default:
       return ERROR_API;
   }
