78e743dd77a9e9fcd59fa000b772e5b5414b4df5
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-08 16:07:25 +0200

    removing logs

--- src/js/ui/searchFeed/searchFeed.mjs
+++ src/js/ui/searchFeed/searchFeed.mjs
@@ -1,7 +1,6 @@
 import { renderCards } from "../../render/renderCards.mjs";
 
 const searchForm = document.querySelector(".search-bar");
-console.log(searchForm);
 const tagToFilter = "snapgo";
 const feedContainer = document.querySelector(".feed-container");
 
@@ -10,7 +9,6 @@ export function searchFeed(posts) {
     event.preventDefault();
     let searchInput = document.querySelector(".search-bar input").value.trim();
     const filteredPosts = posts.filter(post => post.tags.includes(tagToFilter) && post.title.includes(searchInput));
-    console.log(filteredPosts);
     feedContainer.innerHTML = "";
     renderCards(filteredPosts, feedContainer);
   });
