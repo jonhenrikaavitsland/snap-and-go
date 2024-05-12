70e584b6a5cb5c70c38f01cad6331fe45a966001
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-12 22:04:21 +0200

    remove logs

--- src/js/ui/breadCrumb/updateBreadcrumbActive.mjs
+++ src/js/ui/breadCrumb/updateBreadcrumbActive.mjs
@@ -1,7 +1,6 @@
 import { getID } from "../../data/getID.mjs";
 
 const breadCrumbActiveItem = document.querySelector(".breadcrumb-item.active");
-console.log(breadCrumbActiveItem);
 
 export function updateBreadcrumbActive() {
   const id = getID("id");
