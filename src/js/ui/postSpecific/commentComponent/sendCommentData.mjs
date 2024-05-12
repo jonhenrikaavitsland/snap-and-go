9ed28f680ed4ece397f37abd1c6807eeb5b01288
RamonaXR <Ramonaelise@gmail.com>
2024-05-12 17:32:26 +0200

    Adding jsDocs and removing console logs

--- src/js/ui/postSpecific/commentComponent/sendCommentData.mjs
+++ src/js/ui/postSpecific/commentComponent/sendCommentData.mjs
@@ -1,10 +1,10 @@
 import { getID } from "../../../data/getID.mjs";
 import { collectCommentData } from "./collectCommentData.mjs";
 
-
+/**
+ * Submits a comment for a specific post based on the post ID retrieved from the URL.
+ */
 export async function sendCommentData() {
     const id = getID("id");
-    console.log("id:", id);
     let commentObject = collectCommentData(id);
-    console.log("commentObject:", commentObject);
 }
