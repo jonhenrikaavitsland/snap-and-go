8c993898c6a848d1f97f54b2dfd76c9904bdc987
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-13 18:37:33 +0200

    post path added to profile images

--- src/js/ui/profile/displayPosts.mjs
+++ src/js/ui/profile/displayPosts.mjs
@@ -7,18 +7,24 @@ export async function displayPosts(user) {
   imageContainer.className = "row row-profile";
   contentContainer.innerHTML = "";
   contentContainer.classList.remove("w-50");
-  console.log(posts);
+  console.log("this is posts from displayPosts: ", posts);
   console.log(Object.keys(posts.data).length);
 
   for (let i = 0; i < Object.keys(posts.data).length; i++) {
     const col = document.createElement("div");
     col.className = "col-sm-4 gap-2 profile-image-container";
 
+    const anchor = document.createElement("a");
+    anchor.href = `/feed/post/?id=${posts.data[i].id}`;
+
     const img = document.createElement("img");
     img.src = posts.data[i].media.url;
     img.className = "profile-gallery-img";
-    col.append(img);
+
+    anchor.append(img);
+    col.append(anchor);
     imageContainer.append(col);
-    contentContainer.append(imageContainer);
   }
+
+  contentContainer.append(imageContainer);
 }
