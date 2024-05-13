d5c64374b84016b077e1f598e2b4c5dd7afc1169
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-13 20:28:29 +0200

    Adding logout function and h1

--- src/js/pages/profile.mjs
+++ src/js/pages/profile.mjs
@@ -1,5 +1,7 @@
 import { updateURLSettings } from "../ui/editProfile/settingsUrlUpdate.mjs";
+import { logOut } from "../ui/logOut/logOut.mjs";
 import { loadProfile } from "../ui/profile/loadProfile.mjs";
 
 loadProfile();
 updateURLSettings();
+logOut();
