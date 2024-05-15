4710c958f5ac707d153fa43fe0e604bb5645aa9f
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-15 11:14:23 +0200

    Refresh automatic on details saved

--- src/js/ui/profile/settings/handleDetailsSubmit.mjs
+++ src/js/ui/profile/settings/handleDetailsSubmit.mjs
@@ -1,12 +1,17 @@
 import { makeFormData } from "./makeFormData.mjs";
 import { updateSettings } from "./updateSettings.mjs";
 
-export function handleDetailsSubmit(event, user) {
+export async function handleDetailsSubmit(event, user) {
   event.preventDefault();
   console.log("this is event", event);
   const formData = makeFormData();
 
   console.log("this is user", user);
 
-  updateSettings(user, formData);
+  try {
+    await updateSettings(user, formData);
+    window.location.reload();
+  } catch (error) {
+    console.error("Failed to update details: ", error);
+  }
 }
