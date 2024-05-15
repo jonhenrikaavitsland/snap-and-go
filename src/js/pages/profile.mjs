08cbd15187ea49bf97de78c4ce87886e00fd00e6
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-15 10:13:28 +0200

    remove logout function

--- src/js/pages/profile.mjs
+++ src/js/pages/profile.mjs
@@ -1,7 +1,5 @@
 import { updateURLSettings } from "../ui/editProfile/settingsUrlUpdate.mjs";
-import { logOut } from "../ui/logOut/logOut.mjs";
 import { loadProfile } from "../ui/profile/loadProfile.mjs";
 
 loadProfile();
 updateURLSettings();
-logOut();
--- src/js/pages/profileSettings.mjs
+++ src/js/pages/profileSettings.mjs
@@ -1,7 +1,6 @@
 import { propertiesToVariables } from "../localStorage/getValue/propertiesToVariables.mjs";
 import { personalProfileInfo } from "../ui/editProfile/profileInfo.mjs";
 import { getObject } from "../localStorage/getValue/getObject.mjs";
-import { logOut } from "../ui/logOut/logOut.mjs";
 import { setupTabListeners } from "../listener/tabListeners.mjs";
 import { displayAvatars } from "../ui/profile/settings/avatarSelection.mjs";
 const object = getObject();
@@ -10,4 +9,3 @@ const { name, bio, imageSrc } = propertiesToVariables(object);
 personalProfileInfo(name, bio, imageSrc);
 displayAvatars();
 setupTabListeners(name);
-logOut();
