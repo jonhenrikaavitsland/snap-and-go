ed921ea266b3044b2066192a019c4942e9baf4a7
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-15 09:53:39 +0200

    refactoring

--- src/js/ui/logOut/logOut.mjs
+++ src/js/ui/logOut/logOut.mjs
@@ -1,14 +1,16 @@
 import { pageRedirect } from "../../data/pageRedirect.mjs";
-import { clearUserSession } from "../../localStorage/session/clearUserSession.mjs";
 
 const logOutLink = document.querySelector(".nav-log-out");
 
 export function logOut() {
-  if (location.pathname !== "/" || location.pathname !== "/register/") {
-    logOutLink.addEventListener("click", function () {
-      clearUserSession();
-      localStorage.removeItem("alertChanges");
-      pageRedirect("/");
+  if (location.pathname === "/" || location.pathname === "/register") {
+    return;
+  } else {
+    logOutLink.addEventListener("click", () => {
+      localStorage.clear();
+      setTimeout(() => {
+        pageRedirect("/");
+      }, 1000);
     });
   }
 }
