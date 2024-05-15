78244bf11ee850e540d6313f1d652d5730749915
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-15 12:27:59 +0200

    added placeholders

--- src/js/ui/editProfile/profileInfo.mjs
+++ src/js/ui/editProfile/profileInfo.mjs
@@ -10,11 +10,18 @@
  * @param {string} image - The web address for the user's picture.
  */
 export function personalProfileInfo(name, bio, image) {
-  const userName = document.querySelector(".user-bio section h1");
+  const userName = document.querySelector(".user-bio section h2");
   const userBio = document.querySelector(".user-bio section div p");
   const userImage = document.querySelector(".user-bio div img");
+
+  const followButton = document.querySelector(".follow-button");
 
   userName.textContent = "@" + name;
   userBio.textContent = bio;
   userImage.src = image;
+
+  userName.classList.remove("placeholder", "col-4");
+  userImage.classList.remove("placeholder");
+  userBio.classList.remove("placeholder", "col-6");
+  followButton.classList.remove("placeholder");
 }
