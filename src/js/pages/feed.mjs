55c6c25d11aed29a638d4e5d2b1a5bbee41a2f63
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-15 18:22:47 +0200

    remove useless import

--- src/js/pages/feed.mjs
+++ src/js/pages/feed.mjs
@@ -4,7 +4,6 @@ import { renderError } from "../errorHandling/renderError.mjs";
 import { renderCards } from "../render/renderCards.mjs";
 import { checkTextInputLength } from "../ui/createPost/checkTextInputLength.mjs";
 import { createPost } from "../ui/createPost/createPost.mjs";
-import { logOut } from "../ui/logOut/logOut.mjs";
 import { searchFeed } from "../ui/searchFeed/searchFeed.mjs";
 import { sortFeed } from "../ui/sortFeed/sortFeed.mjs";
 import { buttonAction } from "../ui/viewMoreBtnFeedPage/buttonAction.mjs";
