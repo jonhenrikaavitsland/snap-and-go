1b6b838ddf7aafc490141d24d8c7719415d0dfdd
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-15 11:14:40 +0200

    typo

--- src/js/ui/profile/settings/updateAvatar.mjs
+++ src/js/ui/profile/settings/updateAvatar.mjs
@@ -12,11 +12,11 @@ export async function updateAvatar(avatarUrl) {
   const profile = load("profile");
   const profileName = profile.name;
   console.log("this is profileName: ", profileName);
-  // updateSettings(profileName, formData);
+
   try {
     await updateSettings(profileName, formData);
     window.location.reload();
-  } catch {
+  } catch (error) {
     console.error("Failed to update avatar: ", error);
   }
 }
