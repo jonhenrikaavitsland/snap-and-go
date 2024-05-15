6b7d9bc8c59207ca01b0aa47e9a93fc995b5c343
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-05-15 10:13:12 +0200

    Change button color on darkmode

--- src/js/ui/profile/update/updateTab.mjs
+++ src/js/ui/profile/update/updateTab.mjs
@@ -1,10 +1,17 @@
+import { load } from "../../../localStorage/load.mjs";
+
 export function updateActiveTab(activeTab) {
   const tabs = document.querySelectorAll(".btn-group .btn");
   tabs.forEach((tab) => {
     tab.classList.remove("btn-secondary", "z-3");
+    tab.style.backgroundColor = "";
     tab.classList.add("btn-light");
   });
 
   activeTab.classList.add("btn-secondary", "z-3");
   activeTab.classList.remove("btn-light");
+
+  if (load("darkMode")) {
+    activeTab.style.backgroundColor = "#FF8B8B";
+  }
 }
