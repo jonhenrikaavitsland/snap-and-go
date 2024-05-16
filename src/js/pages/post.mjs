d2231d7b783e5fa4cbd9462027af2c0f98e70954
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-16 17:31:02 +0200

    removed old logout function that has been replaced

--- src/js/pages/post.mjs
+++ src/js/pages/post.mjs
@@ -1,5 +1,4 @@
 import { updateBreadcrumbActive } from "../ui/breadCrumb/updateBreadcrumbActive.mjs";
-import { logOut } from "../ui/logOut/logOut.mjs";
 import { sendCommentData } from "../ui/postSpecific/commentComponent/sendCommentData.mjs";
 import { loadPost } from "../ui/postSpecific/loadPost.mjs";
 import { reactToPost } from "../data/API/reactToPost.mjs";
@@ -8,4 +7,3 @@ loadPost();
 setTimeout(sendCommentData, 3000);
 updateBreadcrumbActive();
 setTimeout(reactToPost, 1000);
-logOut();
