906ab2609ac882a605a8c4ff83bf4adc86b37fd0
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-16 20:27:19 +0200

    working on the gallery

--- src/js/ui/gallery/createImgList.mjs
+++ src/js/ui/gallery/createImgList.mjs
@@ -1,11 +1,18 @@
 import { pageRedirect } from "../../data/pageRedirect.mjs";
 
 export function createImgList(post) {
-  const element = document.createElement("img");
-  element.src = post.url;
-  element.alt = "gallery img";
-  element.addEventListener("click", () => {
+  const element = document.createElement("div");
+  element.classList.add("ratio", "ratio-1x1");
+
+  const image = document.createElement("img");
+  image.classList.add("img-fluid", "object-fit-cover");
+  image.src = post.url;
+  image.alt = "gallery img";
+  image.addEventListener("click", () => {
     pageRedirect(`/feed/post/?id=${post.id}`);
   });
+
+  element.append(image);
+
   return element;
 }
