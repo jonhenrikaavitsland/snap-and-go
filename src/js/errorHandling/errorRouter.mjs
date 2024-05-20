b523483e4cc9a3a14afad4bda4efd32ee99b359d
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-20 09:05:18 +0200

    reworked the JSDocs of this file to make sure at least one function meets the requirement

--- src/js/errorHandling/errorRouter.mjs
+++ src/js/errorHandling/errorRouter.mjs
@@ -1,5 +1,17 @@
 import { ERROR_API, ERROR_API_400, ERROR_API_401 } from "./errors.mjs";
 
+/**
+ * This function takes in an error status code and returns a new error response that fits the error status.
+ * @param {string} error 
+ * @returns {variable} a pre-defined error
+ * @example
+ * ```js
+ * // receive error code and return correct error
+ * error === 400;
+ * return new Error("Bad request, please ensure that your inputs are correct.");
+ * // The returned error is passed as a variable
+ * ```
+ */
 export function errorRouter(error) {
   switch (error) {
     case 400:
