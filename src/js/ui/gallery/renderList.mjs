35c62ecd12f36f1923114346b780c5ea20aa9865
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-05-20 08:17:43 +0200

    removing console logs

--- src/js/ui/gallery/renderList.mjs
+++ src/js/ui/gallery/renderList.mjs
@@ -1,7 +1,6 @@
 import { createImgList } from "./createImgList.mjs";
 
 export function renderList(listOfPosts, parent) {
-  console.log("POSTS:", listOfPosts);
   listOfPosts.forEach(post => {
     const finalPost = createImgList(post);
     parent.append(finalPost);
